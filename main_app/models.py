22589f7017f67b33ce3aaf4b529adb73f2179020
Dennelle <dennelle.walton@gmail.com>
2023-12-15 11:43:46 -0500

    updated baby model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Baby(models.Model):
+    # models.CharField are called field types if you want to google others
+	name = models.CharField(max_length=100)
+	breed = models.CharField(max_length=100)
+	description = models.TextField(max_length=250)
+	age = models.IntegerField()# Many to Many, note Toy must be defined before we reference it
+	toys = models.ManyToManyField(Toy)
+	user = models.ForeignKey(User, on_delete=User)
+
+        def get_absolute_url(self):
+		# self.id refers to the cat you just created
+		# this redirects the user after they created something
+		# or updated the cat.
+		return reverse('detail', kwargs={'cat_id': self.id})
