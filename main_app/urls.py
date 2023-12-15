5bd1b0503704ba613acfd4fbeb2bde4dec3d9854
Henry Smith <h.f.smithz99@gmail.com>
2023-12-15 12:02:26 -0500

    added new urls for baby model create, update, delete, index, detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('babys/', views.babies_index, name='index'),
+    path('babys/<int:baby_id>/', views.babies_detail, name='detail'),
+    path('babys/create/', views.BabyCreate.as_view(), name="baby_create"),
+    path('babys/<int:pk>/update/', views.BabyUpdate.as_view(), name='baby_update'),
+    path('babys/<int:pk>/delete/', views.BabyDelete.as_view(), name='baby_delete'),
     
 ]
