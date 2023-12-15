bf13c3070b083f7d14563fc9e142784964e9eb9b
Henry Smith <h.f.smithz99@gmail.com>
2023-12-15 15:03:32 -0500

    updated urls and login.html and signup.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,16 +4,17 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('babys/', views.babies_index, name='index'),
-    path('babys/<int:baby_id>/', views.babies_detail, name='detail'),
-    path('babys/create/', views.BabyCreate.as_view(), name="baby_create"),
-    path('babys/<int:pk>/update/', views.BabyUpdate.as_view(), name='baby_update'),
-    path('babys/<int:pk>/delete/', views.BabyDelete.as_view(), name='baby_delete'),
+    path('babies/', views.babies_index, name='index'),
+    path('babies/<int:baby_id>/', views.babies_detail, name='detail'),
+    path('babies/create/', views.BabyCreate.as_view(), name="baby_create"),
+    path('babies/<int:pk>/update/', views.BabyUpdate.as_view(), name='baby_update'),
+    path('babies/<int:pk>/delete/', views.BabyDelete.as_view(), name='baby_delete'),
     path('playdates/', views.PlayDateList.as_view(), name="playdates_index"),
     path('playdates/<int:pk>/', views.PlayDateDetail.as_view(), name="playdates_detail"),
     path('playdates/create/', views.PlayDateCreate.as_view(), name="playdates_create"),
     path('playdates/<int:pk>/update', views.PlayDateUpdate.as_view(), name="playdates_update"),
     path('playdates/<int:pk>/delete/', views.PlayDateDelete.as_view(), name="playdates_delete"),
-    path('babys/<int:baby_id>/assoc_playdate/<int:playdate_id>/', views.assoc_playdate, name="assoc_playdate"),
+    path('babies/<int:baby_id>/assoc_playdate/<int:playdate_id>/', views.assoc_playdate, name="assoc_playdate"),
+    path('accounts/signup/', views.signup, name="signup"),
     
 ]
