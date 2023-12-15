d0b50f518ea99a337a3d907a9c8d9aa3a57e6ff0
Henry Smith <h.f.smithz99@gmail.com>
2023-12-15 15:11:07 -0500

    added signup function in views

--- main_app/views.py
+++ main_app/views.py
@@ -5,10 +5,31 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Baby, Playdate
 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 # Test from Dennelle
+
+#===SIGN UP FUNCTION===
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid Sign Up - try again!'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+
 
 #=====BABY MODEL CREATE,UPDATE,DELETE FUNCTIONS HERE=====
 #===CREATE FUNCTION===
